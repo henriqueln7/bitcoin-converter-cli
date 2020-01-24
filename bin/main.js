9f467e838451d16246aa64bae09b5a5974e03178
Henrique Lopes <henrique.nobrega@ccc.ufcg.edu.br>
2020-01-24 14:39:42 -0300

    Add binary files

---
+++ bin/main.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+
+const program = require('commander');
+const pkg = require('../package.json');
+const convertBTC = require('./ConvertBTC');
+
+program.version(pkg.version);
+program.description('Convert BTC to any currency defined.');
+
+program.option('-c, --currency <currency>', 'Currency to be converted (Default: USD)', 'USD').option('-a, --amount <amount>', 'Amount to be converted (Default: 1)', 1);
+
+program.parse(process.argv);
+
+convertBTC(program.currency, program.amount);
