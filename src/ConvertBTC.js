9d37f2f3c4cb958605877075da19189bc78b7d0d
Henrique Lopes <henrique.nobrega@ccc.ufcg.edu.br>
2020-01-24 14:39:28 -0300

    Add requests to API to recover the BTC price

---
+++ src/ConvertBTC.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+const chalk = require('chalk');
+const ora = require('ora');
+
+const spinner = ora({
+  text: 'Retrieving bitcoin data',
+  color: 'yellow',
+});
+
+function convertBTC(currency = 'USD', amount = 1) {
+  const url = `https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=${currency}&amount=${amount}`;
+
+  spinner.start();
+
+  return axios.get(url)
+    .then((body) => {
+      spinner.stop();
+      return body.data;
+    })
+    .then((data) => console.info(`${chalk.red(amount)} BTC to ${chalk.cyan(currency)} = ${chalk.yellow(data.price)}`))
+    .catch((err) => {
+      spinner.stop();
+      console.info(chalk.red('Something went wrong in the API. Try in a few minutes.'));
+    });
+}
+
+module.exports = convertBTC;
